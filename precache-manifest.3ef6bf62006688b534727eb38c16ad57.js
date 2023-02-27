c4dce00eac59aea6dff2461f8b8a09a762d851ba
Lucca Araujo <lucca.ara7@gmail.com>
2023-02-27 07:04:00 -0800

    Updates

---
+++ precache-manifest.3ef6bf62006688b534727eb38c16ad57.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fadd87a4a7a469f5bfaf",
+    "url": "/luccara123/DevPortfolio/static/css/main.a71f6571.chunk.css"
+  },
+  {
+    "revision": "fadd87a4a7a469f5bfaf",
+    "url": "/luccara123/DevPortfolio/static/js/main.fadd87a4.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/luccara123/DevPortfolio/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "552890565bba84c90d1b",
+    "url": "/luccara123/DevPortfolio/static/js/2.55289056.chunk.js"
+  },
+  {
+    "revision": "e42b50167d2d70336c66",
+    "url": "/luccara123/DevPortfolio/static/js/runtime~main.e42b5016.js"
+  },
+  {
+    "revision": "9c3afe9de51b684c58ae5a4c40ce17c1",
+    "url": "/luccara123/DevPortfolio/static/media/project-1.9c3afe9d.jpeg"
+  },
+  {
+    "revision": "1dd96c0d25bbefe09a7adcbf2bd53495",
+    "url": "/luccara123/DevPortfolio/static/media/project-2.1dd96c0d.jpeg"
+  },
+  {
+    "revision": "c1e62435ef0a6936714b1fbdcfca4062",
+    "url": "/luccara123/DevPortfolio/static/media/project-3.c1e62435.jpeg"
+  },
+  {
+    "revision": "059913f1abf94b4fdd2643aee95827e7",
+    "url": "/luccara123/DevPortfolio/static/media/project-4.059913f1.jpeg"
+  },
+  {
+    "revision": "6e3fa974f2099c617fafcc87c3f998b8",
+    "url": "/luccara123/DevPortfolio/static/media/project-6.6e3fa974.jpeg"
+  },
+  {
+    "revision": "ceee5ba0c39d6917366f7455afc8c714",
+    "url": "/luccara123/DevPortfolio/static/media/project-7.ceee5ba0.jpeg"
+  },
+  {
+    "revision": "44616c2d631f9e2d3bffe1e1647ba527",
+    "url": "/luccara123/DevPortfolio/static/media/project-8.44616c2d.jpeg"
+  },
+  {
+    "revision": "581de86fd1c561beceec3c0f555b1b73",
+    "url": "/luccara123/DevPortfolio/static/media/testtt1.581de86f.png"
+  },
+  {
+    "revision": "4950087fdb90b54e25271b8ae551fba9",
+    "url": "/luccara123/DevPortfolio/static/media/project-5.4950087f.jpeg"
+  },
+  {
+    "revision": "4950f37e290e4e377274f78a5e9ab9bf",
+    "url": "/luccara123/DevPortfolio/index.html"
+  }
+];
